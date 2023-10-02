1c53892e64cab4af7f5d08803d968ccc5edac6f0
Asief Mahir <asiefmahir1@gmail.com>
2023-10-03 02:24:43 +0600

    FEAT: Introduction to Reusable Components & Props
    Learn about React components, how to write them and how to make them reusable through props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import BioData from "./components/BioData";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BioData 
+        name="Mahir Asief"
+        age="26"
+        gender="Male"
+        email="asiefmahir1@gmail.com"
+        phone="+88 01713121212"
+        address="Rajshahi"
+        country="BD"
+        skills={['React', 'Redux', 'JS']}
+        interests={['Sports', 'theme Development']}
+      />
+			<hr />
+			<BioData 
+        name="Rashed Vai"
+        age="20"
+        gender="Male"
+        email="rashed@gmail.com"
+        phone="+91 9876543210"
+        address="Dhaka"
+        country="BD"
+        skills={['React', 'Redux', 'NodeJs', 'JS', 'WP']}
+        interests={['Music', 'Plugin Development']}
+      />
+			<hr />
+			<BioData 
+        name="Ethila"
+        age="22"
+        gender="Female"
+        email="ethila@gmail.com"
+        address="Khulna"
+        country="BD"
+        skills={['React', 'Redux', 'NodeJs', 'JS', 'WP', 'Python', 'Django']}
+        interests={['Music', 'Plugin Development', 'AI']}
+      />
+		</div>
+	);
 }
 
+
+// App()
+/**
+ * 3 rules to be a component
+ * 1) Component must be a Function
+ * 2) It should return 'something'
+ * 3) This 'something' should be some html-ish code -> JSX
+ */
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// console.log(root.render);
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
+// setTimeout(() => {}, 5000)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
