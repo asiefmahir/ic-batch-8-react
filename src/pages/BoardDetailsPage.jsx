a597e5e95907324841f1565547d921badfe24949
Asief Mahir <asiefmahir1@gmail.com>
2023-11-09 01:31:27 +0600

    feat: Add the Drag & Drop Feature
    Add the Drag and Drop feature which should work when a task in dragged into a different list from another list. And also should work when we sort by drag and drop in the same list. The bug is fixed. Please check the list reducer.

--- src/pages/BoardDetailsPage.jsx
+++ src/pages/BoardDetailsPage.jsx
@@ -1,8 +1,10 @@
 import { useState, useContext } from "react";
 import { useParams, Link } from "react-router-dom";
+import { DragDropContext } from "react-beautiful-dnd";
 
 import { BoardContext } from "../contexts/Board";
 import { ListContext } from "../contexts/List";
+import { TaskContext } from "../contexts/Task";
 
 import AddItem from "../components/AddItem";
 import AddItemForm from "../components/AddItemForm";
@@ -16,6 +18,7 @@ const BoardDetails = () => {
 
 	const { dispatchBoardAction } = useContext(BoardContext);
 	const { lists, dispatchListAction } = useContext(ListContext);
+	const { dispatchTaskAction } = useContext(TaskContext);
 
 	const renderedList = lists.filter((item) => item.boardId === boardId);
 
@@ -44,28 +47,64 @@ const BoardDetails = () => {
 		setListTitle("");
 	};
 
-	return (
-		<div className="d-flex m-top-sm flex-direction-row">
-			<Link to="/">Back to Boards</Link>
-			{renderedList.map((taskList) => (
-				<TaskList key={taskList.id} taskList={taskList} />
-				// <p key={taskList.id}>{taskList.title}</p>
-			))}
+	const dragHandler = (result) => {
+		const { draggableId, source, destination } = result;
+		// console.log(result);
+		if (!destination) {
+			return;
+		}
 
-			{!editMode ? (
-				<AddItem listAddItem={true} setEditMode={setEditMode} />
-			) : (
-				<AddItemForm
-					title={listTitle}
-					listForm={true}
-					onChangeHandler={(e) => {
-						setListTitle(e.target.value);
-					}}
-					setEditMode={setEditMode}
-					submitHandler={submitHandler}
-				/>
-			)}
-		</div>
+		if (
+			source.droppableId === destination.droppableId &&
+			source.index === destination.index
+		) {
+			return;
+		}
+
+		if (source.droppableId !== destination.droppableId) {
+			dispatchTaskAction({
+				type: "CHANGE_LIST_ID_OF_TASK",
+				payload: {
+					id: draggableId,
+					listId: destination.droppableId,
+				},
+			});
+		}
+
+		dispatchListAction({
+			type: "SORT_TASK_IDS_IN_LIST",
+			payload: {
+				draggableId,
+				source,
+				destination,
+			},
+		});
+	};
+
+	return (
+		<DragDropContext onDragEnd={dragHandler}>
+			<div className="d-flex m-top-sm flex-direction-row">
+				<Link to="/">Back to Boards</Link>
+				{renderedList.map((taskList) => (
+					<TaskList key={taskList.id} taskList={taskList} />
+					// <p key={taskList.id}>{taskList.title}</p>
+				))}
+
+				{!editMode ? (
+					<AddItem listAddItem={true} setEditMode={setEditMode} />
+				) : (
+					<AddItemForm
+						title={listTitle}
+						listForm={true}
+						onChangeHandler={(e) => {
+							setListTitle(e.target.value);
+						}}
+						setEditMode={setEditMode}
+						submitHandler={submitHandler}
+					/>
+				)}
+			</div>
+		</DragDropContext>
 	);
 };
 
