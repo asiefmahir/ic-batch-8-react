8bb163a33cb3860c281fe4a2ee567ebcf245a558
Asief Mahir <asiefmahir1@gmail.com>
2023-11-09 01:31:27 +0600

    feat: Add the Drag & Drop Feature
    Add the Drag and Drop feature which should work when a task in dragged into a different list from another list. And also should work when we sort by drag and drop in the same list. The bug is fixed. Please check the list reducer.

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,4 +1,5 @@
 import { useState, useContext } from "react";
+import { Droppable } from "react-beautiful-dnd";
 
 import { icons } from "../assets";
 
@@ -8,7 +9,7 @@ import AddItemForm from "./AddItemForm";
 import { BoardContext } from "../contexts/Board";
 import { ListContext } from "../contexts/List";
 import { TaskContext } from "../contexts/Task";
-// import TaskCard from "./TaskCard";
+import TaskCard from "./TaskCard";
 
 const TaskList = ({ taskList }) => {
 	const [editMode, setEditMode] = useState(false);
@@ -69,37 +70,56 @@ const TaskList = ({ taskList }) => {
 	};
 
 	return (
-		<div className="list-container">
-			<div className="list-title-container">
-				<h5>{taskList.title}</h5>
-				<img
-					onClick={removeHandler}
-					src={icons.crossIcon}
-					alt=""
-					className="add-item-icon"
-				/>
-			</div>
-			{taskList.tasks
-				.map((item) => {
-					return allTasks.find((ele) => ele.id === item);
-				})
-				.map((task) => (
-					// <TaskCard key = {task.id} task={task}/>
-					<p key={task.id}>{task.title}</p>
-				))}
-			{!editMode ? (
-				<AddItem setEditMode={setEditMode} />
-			) : (
-				<AddItemForm
-					title={taskTitle}
-					onChangeHandler={(e) => {
-						setTaskTitle(e.target.value);
-					}}
-					setEditMode={setEditMode}
-					submitHandler={submitHandler}
-				/>
-			)}
-		</div>
+		<Droppable droppableId={taskList.id}>
+			{(provided) => {
+				// console.log(provided);
+
+				return (
+					<div
+						{...provided.droppableProps}
+						className="list-container"
+						ref={provided.innerRef}
+					>
+						<div className="list-title-container">
+							<h5>{taskList.title}</h5>
+							<img
+								onClick={removeHandler}
+								src={icons.crossIcon}
+								alt=""
+								className="add-item-icon"
+							/>
+						</div>
+						{taskList.tasks
+							.map((item) => {
+								//['task -1', 'task-2']
+								// ['task-2', 'task-1']
+								return allTasks.find((ele) => ele.id === item);
+							})
+							.map((task, index) => (
+								<TaskCard
+									key={task.id}
+									task={task}
+									index={index}
+								/>
+								// <p key={task.id}>{task.title}</p>
+							))}
+						{provided.placeholder}
+						{!editMode ? (
+							<AddItem setEditMode={setEditMode} />
+						) : (
+							<AddItemForm
+								title={taskTitle}
+								onChangeHandler={(e) => {
+									setTaskTitle(e.target.value);
+								}}
+								setEditMode={setEditMode}
+								submitHandler={submitHandler}
+							/>
+						)}
+					</div>
+				);
+			}}
+		</Droppable>
 	);
 };
 
