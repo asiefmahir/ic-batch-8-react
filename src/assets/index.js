f4c6b4b888d423fb8722d72ce24a2237f79f58ee
Asief Mahir <asiefmahir1@gmail.com>
2023-12-03 22:00:40 +0600

    feat: Learn about how to convert traditional React Apps into Next Js
    Learn about converting Raw React Apps into Next Js. Learn what is Incremental Static Re-generation (ISR), why it is needed and how to implement it in our next apps

---
+++ src/assets/index.js
@@ -0,0 +1,18 @@
+import crossIcon from './icons/cross.png';
+import plusIcon from './icons/plus.svg';
+import minusIcon from './icons/minus.svg';
+import editIcon from './icons/edit-icon.png'
+
+const icons  = {
+    crossIcon,
+    plusIcon,
+    minusIcon,
+    editIcon
+}
+
+const images = {};
+
+export {
+    icons,
+    images
+}
