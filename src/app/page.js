31cec4861796d5f8290db315efcc906a3600dc37
Asief Mahir <asiefmahir1@gmail.com>
2023-12-23 22:58:46 +0600

    feat: Learn about pagination, filtering techniques in Next Js
    TODO: We will add more filtering options and styles to our app

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,31 @@ import { authOptions } from "@/utils/auth";
 import connectDb from "@/utils/connectDb";
 
 import GridLayout from "@/components/home/GridLayout";
+import Pagination from "@/components/product/Pagination";
 
-export default async function Home() {
+const getProducts = async (searchParams) => {
+	const searchQuery = new URLSearchParams({
+		page: searchParams?.page || 1,
+	}).toString();
+
+	const res = await fetch(
+		`http://localhost:3000/api/product?${searchQuery}`,
+		{
+			method: "GET",
+		},
+	);
+	if (!res.ok) {
+		throw new Error("Failed to fetch products");
+	}
+	const data = await res.json();
+	return data;
+};
+export default async function Home({ searchParams }) {
+	console.log(searchParams);
 	await connectDb();
-	const products = await Product.find({}).sort({ createdAt: -1 });
+	const { products, currentPage, totalPages } = await getProducts(
+		searchParams,
+	);
 	return (
 		<main>
 			<div>
@@ -18,19 +39,24 @@ export default async function Home() {
 						</div>
 					</div>
 				</div>
-				<div class="section">
-					<div class="container">
-						<div class="section__head">
-							<div class="product__details__title">
+				<div className="section">
+					<div className="container">
+						<div className="section__head">
+							<div className="product__details__title">
 								<h2>Filtered Products</h2>
 							</div>
 						</div>
-						<div class="section__content">
+						<div className="section__content">
 							<GridLayout products={products} />
 						</div>
 					</div>
 				</div>
 			</div>
+			<Pagination
+				currentPage={currentPage}
+				totalPages={totalPages}
+				pathname={"/"}
+			/>
 		</main>
 	);
 }
